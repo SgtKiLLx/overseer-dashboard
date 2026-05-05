55adb39cd913fa4ee8757585fcd93526ef2f5a34
SgtKiLLx <70082342+SgtKiLLx@users.noreply.github.com>
2026-05-05 18:18:59 -0400

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // This allows images from Discord to show up (like your profile pic)
+    images: {
+        domains: ['cdn.discordapp.com'],
+    },
+};
+
+export default nextConfig;
